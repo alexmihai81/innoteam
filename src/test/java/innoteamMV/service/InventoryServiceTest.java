55c7e5afb0953a36affa29e659318a113b8ebea7
MihaiChirvasiu <mihaichirvasiu3@gmail.com>
2023-03-28 10:56:09 +0300

    Updated JUnit 5 Tests

--- src/test/java/innoteamMV/service/InventoryServiceTest.java
+++ src/test/java/innoteamMV/service/InventoryServiceTest.java
@@ -41,40 +41,57 @@ class InventoryServiceTest {
     }
 
     @Test
+    @Tag("testeValideBVA")
     void addInhousePartValid() {
-        assertEquals("", Part.isValidPart("M", 10, 2, 1, 3, ""));
-        assertEquals("", Part.isValidPart("M123", 10, 2, 1, 3, ""));
+        assertEquals("", Part.isValidPart("M", Float.MAX_VALUE, 2, 1, 3, ""));
+        StringBuilder nameS = new StringBuilder();
+        for(int i = 0; i < 255; i++){
+            nameS.append("a");
+        }
+        assertEquals("", Part.isValidPart(nameS.toString(), 10, 2, 1, 3, ""));
     }
 
     @Test
+    @Tag("testeInvalideBVA")
     void addPartInvalid() {
         assertEquals("A name has not been entered. ", Part.isValidPart("", 10, 2, 1, 3, ""));
-        assertEquals("The price must be greater than 0. ", Part.isValidPart("M123", -1, 2, 1, 3, ""));
+        assertEquals("The price must be greater than 0. ", Part.isValidPart("M123", 0, 2, 1, 3, ""));
     }
 
     @Test
+    @Tag("testValideECP")
     void addOutsourcePart() {
         assertEquals("Piesa 1", part.getName());
         assertEquals("Piesa 2", part2.getName());
         assertEquals("A", part2.getCompanyName());
+        assertEquals("", Part.isValidPart("Piesa 2", 100, 3, 1, 4, ""));
         assertEquals("", Part.isValidPart(name, price, inStock, min, max, ""));
     }
 
     @RepeatedTest(2)
+    @Tag("testeInvalideECP")
     void addOutsourcePartInvalid(){
         assertEquals("Inventory level is higher than the maximum value. ", Part.isValidPart(name, price, 10, min, max, ""));
     }
 
     @ParameterizedTest
     @MethodSource
+    @Tag("testeInvalideECP")
     void addPart3(String name, double price, int inStock, int min, int max) {
         assertEquals("A name has not been entered. ", Part.isValidPart(name, price, inStock, min, max, ""));
     }
-
+    @Tag("generatorParameterized")
     private static Stream<Arguments> addPart3() {
         return Stream.of(
                 arguments("", 10, 3, 1, 4)
         );
+    }
+    @Tag("test4")
+    @Test
+    void isValiPart4(){
+        assertEquals("The Min value must be less than the Max value. Inventory level is higher than the maximum value. ",
+                Part.isValidPart("Piesa", 10, 4, 3, 1, ""));
+
     }
 
     @Disabled
