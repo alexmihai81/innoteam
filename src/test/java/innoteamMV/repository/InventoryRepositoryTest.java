8bd49ded4d7538388aba5f3c4faa1b2de7c5a6c5
MihaiChirvasiu <mihaichirvasiu3@gmail.com>
2023-04-12 14:38:22 +0300

    Updated Tests

--- src/test/java/innoteamMV/repository/InventoryRepositoryTest.java
+++ src/test/java/innoteamMV/repository/InventoryRepositoryTest.java
@@ -1,5 +1,6 @@
 package innoteamMV.repository;
 
+import innoteamMV.model.Product;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -41,23 +42,55 @@ class InventoryRepositoryTest {
     @Tag("TestCase4")
     void TC4(){
         assertNotNull(repository.getProductFromString("P,5,products4,11.0,2,1,10, "));
+        Product product = repository.getProductFromString("P,5,products4,11.0,2,1,10, ");
+        assertEquals(product.getProductId(), 5);
+        assertEquals(product.getName(), "products4");
+        assertEquals(product.getPrice(), 11.0);
+        assertEquals(product.getMax(), 10);
+        assertEquals(product.getMin(), 1);
+        assertEquals(product.getInStock(), 2);
+        assertEquals(product.getAssociatedParts().size(), 0);
     }
 
     @Test
     @Tag("TestCase5")
     void TC5(){
         assertNotNull(repository.getProductFromString("P,6,products4,11.0,2,1,10,1"));
+        Product product = repository.getProductFromString("P,6,products4,11.0,2,1,10,1");
+        assertEquals(product.getProductId(), 6);
+        assertEquals(product.getName(), "products4");
+        assertEquals(product.getPrice(), 11.0);
+        assertEquals(product.getMax(), 10);
+        assertEquals(product.getMin(), 1);
+        assertEquals(product.getInStock(), 2);
+        assertEquals(product.getAssociatedParts().size(), 1);
     }
 
     @Test
     @Tag("TestCase6")
     void TC6(){
         assertNotNull(repository.getProductFromString("P,6,products4,11.0,2,1,10,100"));
+        Product product = repository.getProductFromString("P,6,products4,11.0,2,1,10,100");
+        assertEquals(product.getProductId(), 6);
+        assertEquals(product.getName(), "products4");
+        assertEquals(product.getPrice(), 11.0);
+        assertEquals(product.getMax(), 10);
+        assertEquals(product.getMin(), 1);
+        assertEquals(product.getInStock(), 2);
+        assertEquals(product.getAssociatedParts().size(), 1);
     }
 
     @Test
     @Tag("TestCase7")
     void TC7(){
         assertNotNull(repository.getProductFromString("P,4,products4,11.0,2,1,10,5:8:7:7"));
+        Product product = repository.getProductFromString("P,6,products4,11.0,2,1,10,5:8:7:7");
+        assertEquals(product.getProductId(), 6);
+        assertEquals(product.getName(), "products4");
+        assertEquals(product.getPrice(), 11.0);
+        assertEquals(product.getMax(), 10);
+        assertEquals(product.getMin(), 1);
+        assertEquals(product.getInStock(), 2);
+        assertEquals(product.getAssociatedParts().size(), 4);
     }
 }
